78d3c729d45ffb11f27845008121df055cdc13b0
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-07-04 22:14:39 +0200

    Refactoring

--- packages/plugin-observer/src/index.ts
+++ packages/plugin-observer/src/index.ts
@@ -1,13 +1,14 @@
-import {LogData as CoreLogData} from '@spryrocks/logging-browser-core';
 import {
   ILoggerObserver as CoreILoggerObserver,
   LoggerObserver as CoreLoggerObserver,
 } from '@spryrocks/logging-browser-observer';
+import {LogData as CoreLogData} from '@spryrocks/logging-browser-core';
 
 export type LogData = CoreLogData & {plugin: string; action: string | undefined};
 
 export type ILoggerObserver<TLogData extends LogData = LogData> =
   CoreILoggerObserver<TLogData>;
 
-export type LoggerObserver<TLogData extends LogData = LogData> =
-  CoreLoggerObserver<TLogData>;
+export class LoggerObserver<
+  TLogData extends LogData = LogData,
+> extends CoreLoggerObserver<TLogData> {}
