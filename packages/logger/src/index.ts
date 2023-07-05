1e8248ea50fc1a174274025773e7665af43c6272
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-07-05 17:45:53 +0200

    Refactoring

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,4 +1,4 @@
 export {ILogger} from './ILogger';
 export {ILoggerFactory} from './ILoggerFactory';
 export {LoggerFactory} from './LoggerFactory';
-export {LogData} from '@spryrocks/logger-observer';
+export {LogData, LoggerObserver} from '@spryrocks/logger-observer';
