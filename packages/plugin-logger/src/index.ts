2e486eaffbfd518b112fb0d11ce9554fbe4db9e1
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-07-05 17:55:42 +0200

    Refactoring

--- packages/plugin-logger/src/index.ts
+++ packages/plugin-logger/src/index.ts
@@ -1,12 +1,21 @@
 import {LoggerFactory as CoreLoggerFactory, ILoggerFactory} from '@spryrocks/logger';
 import {
   ILoggerObserver,
+  ILoggerReceiver,
   LogData,
   LoggerObserver,
+  LogType,
 } from '@spryrocks/logger-plugin-observer';
 
 export class LoggerFactory<
   TLogData extends LogData = LogData,
 > extends CoreLoggerFactory<TLogData> {}
 
-export {ILoggerFactory, ILoggerObserver, LoggerObserver};
+export {
+  ILoggerFactory,
+  ILoggerObserver,
+  LoggerObserver,
+  ILoggerReceiver,
+  LogData,
+  LogType,
+};
