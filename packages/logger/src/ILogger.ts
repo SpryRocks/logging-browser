37550bad6db8a4c11bff634a16407b52d11861d4
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-07-13 18:02:42 +0200

    Refactoring

--- packages/logger/src/ILogger.ts
+++ packages/logger/src/ILogger.ts
@@ -2,6 +2,8 @@ import {LogParams} from '@spryrocks/logger-observer';
 
 export type TagOptions = {keepParams?: boolean};
 
+export type ChildOptions = {keepParams?: boolean};
+
 export interface ILogger {
   warning(message: string, params?: LogParams): void;
   debug(message: string, params?: LogParams): void;
@@ -9,4 +11,5 @@ export interface ILogger {
   error(error: unknown, message?: string, params?: LogParams): void;
   updateParams(params: LogParams): void;
   tag(tag: string, options?: TagOptions): ILogger;
+  child(options?: ChildOptions): ILogger;
 }
