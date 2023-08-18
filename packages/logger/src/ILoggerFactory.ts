aff08df3cfeafeebcf55a83a3da0f9c671105f24
maksim.zhemerenko <maksym.zhemerenko@paymash.com>
2023-08-18 16:15:45 +0200

    Refactoring

--- packages/logger/src/ILoggerFactory.ts
+++ packages/logger/src/ILoggerFactory.ts
@@ -1,11 +1,11 @@
 import {ILogger} from './ILogger';
 import {LogObjectFormatter} from './ILogFormatter';
 
-export type CreateLoggerOptions<TGlobalData = unknown> = {
+export type CreateLoggerOptions<TGlobalData> = {
   globalData?: Partial<TGlobalData>;
   objectFormatter?: LogObjectFormatter;
 };
 
-export interface ILoggerFactory<TGlobalData> {
+export interface ILoggerFactory<TGlobalData = unknown> {
   createLogger(tag?: string, options?: CreateLoggerOptions<TGlobalData>): ILogger;
 }
